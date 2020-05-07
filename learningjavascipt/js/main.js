3ed611843270e7878d94185a84dce5ecd83ecc48
toffy <oketofoke@gmail.com>
2020-05-07 23:58:30 +0100

    simple react app

--- learningjavascipt/js/main.js
+++ learningjavascipt/js/main.js
@@ -1,3 +1,7 @@
+
+<script crossorigin src="https://unpkg.com/react@16/umd/react.development.js"></script>
+<script crossorigin src="https://unpkg.com/react-dom@16/umd/react-dom.development.js"></script>
+
 // var newItemCounter = 1;
 // var newList = document.getElementById('our-list');
 // var but = document.getElementById('our-button');
